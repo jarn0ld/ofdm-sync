b7ce2b7a55188e66e9e11063863b0ade1a5d0e31
Julian Arnold <julian.arnold@ettus.com>
2015-02-10 09:11:15 -0800

    changed from averaging to using each pilot on a separate range

--- octave-sym/derotate_ofdm_symbol.m
+++ octave-sym/derotate_ofdm_symbol.m
@@ -21,21 +21,13 @@
 
 ## Author: Julian Arnold <julian@juarnold-t440>
 ## Created: 2015-02-06
-
+## Pilots in carrier -21, -7, 7, 21
 ## Pilots are 1 1 1 -1 modulated by the polarity bit
 
-function [S_out, phi_total_new, phi_error_avg] = derotate_ofdm_symbol (S, S_pilots, phi_old, polarity)
-  S = S .* exp(-j*(phi_old));
-  S_pilots = S_pilots .* exp(-j*(phi_old));
+function [S_out] = derotate_ofdm_symbol (S, S_pilots, polarity)
   Phi_error = [arg(S_pilots(1)*polarity) arg(S_pilots(2)*polarity) arg(S_pilots(3)*polarity) arg(-1*(S_pilots(4)*polarity))];
-  phi_error_diff = (arg(S_pilots(2)*polarity) - arg(S_pilots(1)*polarity)) / 14
-  phi_error_avg = mean(Phi_error)
-
-  S_out = S .* exp(-j.*(phi_error_avg));
+  #Averaging pilots  results in weierd behaviour... E.g.  + + + - can result in problems
+  #Therefre use this methode or use phase tracking so that the phase error can not become too large
+  S_out = [S(1:14).*exp(-j.*(Phi_error(1))) S(15:28).*exp(-j.*(Phi_error(2))) S(29:42).*exp(-j.*(Phi_error(3))) S(43:end).*exp(-j.*(Phi_error(4)))];
   
-  #for ii = 1:length(S)
-  #  S_out(ii) = S(ii) .* exp(-j*(phi_error_diff)*(ii-1));
-  #endfor
-  
-  phi_total_new = phi_error_avg + phi_old;
 endfunction
